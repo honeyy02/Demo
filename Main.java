3c953a9ea874a80b15f9b2fd58b3bbfc57909f43
Honey Sharma <113124234+honeyy02@users.noreply.github.com>
2024-07-08 12:34:47 +0530

    Update4 Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("This is change after automate the build");
+    System.out.println"This is to test email notification for fail build");
+    
   }
 }
