c6a691353ca3fb656084b1e841ffd350b089d75f
Honey Sharma <113124234+honeyy02@users.noreply.github.com>
2024-07-26 12:31:21 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     System.out.println("Hello World");
     //System.out.println("This is change after automate the build");
     //System.out.println("This is to test email notification for fail build");
-    System.put.println("After the commit the build starts");
+    System.put.println("After the commit the build starts changes are made after making url public");
     
   }
 }
